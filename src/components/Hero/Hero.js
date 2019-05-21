657112cf6893fc0bd2f3bbf9c4aa4946d5d9c36f
Lucas Inocente <olucassanchez@gmail.com>
2019-05-21 00:42:32 -0300

    Usa redux em outra classe apenas para estudo

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,27 @@
-import React from "react";
-import { Container, Hero, HeroBody, Title } from 'bloomer';
+import React, { Component } from "react";
+import { Container, Hero, HeroBody, Title, Subtitle, Button } from 'bloomer';
 
-const hero = (props) => {
+import { connect } from 'react-redux'
+
+class HeroComponent extends Component {
+  render() {
     return (
       <Hero isColor='info' isSize='medium'>
         <HeroBody>
           <Container hasTextAlign='centered'>
-            <Title>{props.title}</Title>
+            <Title>{this.props.title}</Title>
+            <Subtitle>Total users: {Object.keys(this.props.usrs).length}</Subtitle>
           </Container>
         </HeroBody>
       </Hero>
     )
+  }
 }
 
-export default hero
+const mapStateToProps = state => {
+  return {
+    usrs: state.users
+  }
+}
+
+export default connect(mapStateToProps, null)(HeroComponent)
